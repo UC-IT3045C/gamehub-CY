093dd8635540525006445550eb74b683a3856e13
Calvin Yeboah <Calvinyeb04@gmail.com>
2024-04-13 20:36:24 -0400

    Add HomePage and RPS-Game components

---
+++ pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to GameHub!</h1>
+            <p>Explore a world of gaming adventures.</p>
+        </div>
+    );
+}
+
+export default HomePage;
