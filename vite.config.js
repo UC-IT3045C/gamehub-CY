687868eaa85e69211e2478dd915e8bbdc90cac62
Calvin Yeboah <Calvinyeb04@gmail.com>
2024-04-14 19:54:39 -0400

    Update build configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/gamehub-Calvinyeb04/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: 'src/main.jsx',
-    },
-  },
 })
