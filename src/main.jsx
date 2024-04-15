5ad54b1a944c92254851e54a42be5fe52faf21a9
Calvin Yeboah <Calvinyeb04@gmail.com>
2024-04-14 20:01:11 -0400

    Refactor React Router imports and update routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Layout from './Layout';
 import HomePage from "../pages/HomePage";
@@ -8,12 +10,12 @@ import "../components/Navigation.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="rps" element={<RPSGame />} />
-          <Route path="tictactoe" element={<TicTacToeGame />} />
+          <Route path="/rps" element={<RPSGame />} />
+          <Route path="/tictactoe" element={<TicTacToeGame />} />
         </Routes>
       </Layout>
     </Router>
