d15f51f5d8e51ea28b745fc42aa282635aa5e569
Calvin Yeboah <Calvinyeb04@gmail.com>
2024-04-14 20:16:05 -0400

    Update React Router imports and routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Layout from './Layout';
 import HomePage from "../pages/HomePage";
 import RPSGame from "../pages/rps/RPS-Game";
@@ -10,7 +10,7 @@ import "../components/Navigation.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <Router basename={process.env.PUBLIC_URL}>
       <Layout>
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -18,6 +18,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="/tictactoe" element={<TicTacToeGame />} />
         </Routes>
       </Layout>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
