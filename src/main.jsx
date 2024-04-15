6b4f728639aece92c3a87c263e7d7804f12e1d18
Calvin Yeboah <Calvinyeb04@gmail.com>
2024-04-14 23:01:01 -0400

    No More Errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'; 
 import Layout from './Layout';
 import HomePage from "../pages/HomePage";
 import RPSGame from "../pages/rps/RPS-Game";
@@ -8,16 +8,31 @@ import TicTacToeGame from "../pages/ticTacToe/src/TicTacToe";
 import './index.css';
 import "../components/Navigation.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: `/`,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/rps",
+        element: <RPSGame />,
+      },
+      {
+        path: "/tictactoe",
+        element: <TicTacToeGame />,
+      },
+    ],
+  },
+], { basename: import.meta.env.BASE_URL });
+
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename={import.meta.env.BASE_URL}>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/rps" element={<RPSGame />} />
-          <Route path="/tictactoe" element={<TicTacToeGame />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <RouterProvider router={router}>
+      <Layout />
+    </RouterProvider>
   </React.StrictMode>
 );
