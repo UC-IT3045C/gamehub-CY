07e99cfc6f253b3d6176d8802b17a9b64f3644e6
Calvin Yeboah <Calvinyeb04@gmail.com>
2024-04-18 13:52:16 -0400

    Add 2048 game to navigation and update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import HomePage from "../pages/HomePage";
 import RPSGame from "../pages/rps/RPS-Game";
 import Wordle from "../pages/wordle/wordle";
 import TicTacToeGame from "../pages/ticTacToe/src/TicTacToe";
+import Game2048 from "../pages/2048/2048Game.jsx";
+import '../pages/2048/styles.css';
 import './index.css';
 import "../components/Navigation.css";
 
@@ -29,7 +31,11 @@ const router = createBrowserRouter([
       {
         path: "/wordle",
         element: <Wordle />,
-      }
+      },
+      {
+        path: "/2048",
+        element: <Game2048 />,
+      },
     ],
   },
 ], { basename: import.meta.env.BASE_URL });
